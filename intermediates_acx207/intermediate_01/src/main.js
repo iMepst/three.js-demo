48762c0184df010d60159767bf3b46c9dfe47a7d
Markus Epstein <Markus.Epstein@haw-hamburg.de>
2022-04-25 12:21:33 +0200

    Added Orbit Controls. Added new import 'CONTROLS'. Made a few comments for better understanding. Restructured.

--- intermediates_acx207/intermediate_01/src/main.js
+++ intermediates_acx207/intermediate_01/src/main.js
@@ -1,27 +1,41 @@
 import * as THREE from 'three';
-import * as DATGUI from 'datgui'
+import * as DATGUI from 'datgui';
+import * as CONTROLS from 'controls';
 
 let sphere;
 let cube;
 let plane;
 let pointLight;
+let orbitControls;
 
 function main() {
+  //Initializing the camera, scene and window
   sceneInit();
   cameraInit();
   windowInit();
 
+  //Using the Orbit Controls
+  orbitControls = new CONTROLS.OrbitControls(window.camera, window.renderer.domElement);
+  orbitControls.target = new THREE.Vector3(0,0,0); //replaces window.camera.lookAt(0, 0, 0)
+
   //Integration of the renderer output into the HTML structure
   document.getElementById('3d_content').appendChild(window.renderer.domElement);
 
+  //Resizes the view if the aspect ratio of the window changes
+  window.addEventListener( 'resize', onWindowResize, false );
+
+  //Initializing the geometric objects
   cubeInit();
   sphereInit();
   planeInit();
 
   mainLoop();
+
+  //Initializing the light and the GUI
   pointLightInit();
   guiInit();
-  //globalLight();
+
+  orbitControls.update(); //Activate/acquire the target
 }
 
 window.onload = main; //fired when the entire page loads, including its content
@@ -31,18 +45,23 @@ function mainLoop() {
   requestAnimationFrame(mainLoop); //Request for the next possible execution of the mainLoop()
 }
 
+//Scene, window, camera and GUI functions
 function sceneInit() {
   window.scene = new THREE.Scene(); //Scene graph Object
   window.scene.add(new THREE.AxesHelper(20)); //Length of the Coordinate axes
 }
-
 function windowInit() {
   window.renderer = new THREE.WebGLRenderer({antialias: true}); //Renderer-Object
   window.renderer.setSize(window.innerWidth, window.innerHeight); //Size of the Framebuffer
   window.renderer.setClearColor(0x000000); //Background color of the frame buffer
   window.renderer.shadowMap.enabled = true;
 }
+function onWindowResize(){
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
 
+  renderer.setSize( window.innerWidth, window.innerHeight );
+}
 function cameraInit() {
   //Camera-Object
   window.camera = new THREE.PerspectiveCamera(
@@ -52,38 +71,8 @@ function cameraInit() {
       1000); //Distance of the far-plane
 
   window.camera.position.set(30, 40, 50);
-  window.camera.lookAt(0, 0, 0);
+
 }
-
-function cubeInit() {
-  let cubeGeometry = new THREE.BoxGeometry(5, 5, 5); //Edge lengths
-  let cubeMaterial = new THREE.MeshLambertMaterial({color: 0xff000, wireframe: false});
-  cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-  cube.castShadow = true;
-
-  cube.position.set(-6, 3, 5); //Position in world coordinates
-  window.scene.add(cube); //Add to scene
-}
-
-function sphereInit() {
-  let sphereGeometry = new THREE.SphereGeometry(5, 10, 10);
-  let sphereMaterial = new THREE.MeshLambertMaterial({color: 0x00FFFF, wireframe: false});
-  sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-  sphere.castShadow = true;
-  sphere.position.set(10, 5, -5);
-  window.scene.add(sphere);
-}
-
-function planeInit() {
-  let planeGeometry = new THREE.PlaneGeometry(40, 40);
-  let planeMaterial = new THREE.MeshLambertMaterial({color: 0x888888, side: THREE.DoubleSide, wireframe: false});
-  plane = new THREE.Mesh(planeGeometry, planeMaterial);
-
-  plane.receiveShadow = true;
-  plane.rotation.x = Math.PI / 2;
-  window.scene.add(plane);
-}
-
 function guiInit() {
   let gui = new DATGUI.GUI();
   const cameraFolder = gui.addFolder('Camera');
@@ -104,12 +93,40 @@ function guiInit() {
   gui.add(plane.position,'z', -50, 50);*/
 }
 
+//Object functions
+function cubeInit() {
+  let cubeGeometry = new THREE.BoxGeometry(5, 5, 5); //Edge lengths
+  let cubeMaterial = new THREE.MeshLambertMaterial({color: 0xff000, wireframe: false});
+  cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+  cube.castShadow = true;
+
+  cube.position.set(-6, 3, 5); //Position in world coordinates
+  window.scene.add(cube); //Add to scene
+}
+function sphereInit() {
+  let sphereGeometry = new THREE.SphereGeometry(5, 10, 10);
+  let sphereMaterial = new THREE.MeshLambertMaterial({color: 0x00FFFF, wireframe: false});
+  sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+  sphere.castShadow = true;
+  sphere.position.set(10, 5, -5);
+  window.scene.add(sphere);
+}
+function planeInit() {
+  let planeGeometry = new THREE.PlaneGeometry(40, 40);
+  let planeMaterial = new THREE.MeshLambertMaterial({color: 0x888888, side: THREE.DoubleSide, wireframe: false});
+  plane = new THREE.Mesh(planeGeometry, planeMaterial);
+
+  plane.receiveShadow = true;
+  plane.rotation.x = Math.PI / 2;
+  window.scene.add(plane);
+}
+
+//Light functions
 function ambientLight() {
   let ambientLight = new THREE.AmbientLight(0xFFFFFF);
   ambientLight.intensity = 0.5;
   window.scene.add(ambientLight);
 }
-
 function pointLightInit() {
   pointLight = new THREE.PointLight(0xFFFFFF); //Color value
   pointLight.position.set(15, 20, 20); //Position
